b405addf918e4965283eac1b701184ee4484cb0d
Harikareddi13 <harikareddi3@gmail.com>
2025-10-07 23:04:12 +0530

    Initial commit

---
+++ scripts/checkout.js
@@ -0,0 +1,76 @@
+import { loadProducts, loadProductsFetch } from '../data/products.js';
+import { renderOderSummary } from './checkout/orderSummary.js';
+import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import { loadCart } from '../data/cart.js';
+
+//import '../data/cart-class.js';
+//import '../data/backend-practice.js';
+
+async function loadPage() {
+  try{
+    //throw 'error1';
+
+    await loadProductsFetch();
+  
+    const value = await new Promise((resolve, reject) => {
+      //throw 'error2';
+      loadCart(() => {
+        //reject('error3');
+        resolve();
+      });
+    });
+
+  } catch(error) {
+    console.log('Unexpected error. Please try again later.');
+  }
+  
+  renderOderSummary();
+  renderPaymentSummary();
+}
+loadPage()
+
+/*Promise.all([
+  loadProductsFetch(),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
+  renderOderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('vlaue1');
+  });
+
+}).then((value) => {
+  console.log(value);
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOderSummary();
+  renderPaymentSummary();
+});
+*/
+
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
